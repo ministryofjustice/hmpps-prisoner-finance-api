03de65eff3e597b88410fd93b39889f01a94cb4b
emilyfbgov <emily.bennett@justice.gov.uk>
2026-03-04 16:54:01 +0000

    Update error handling

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceapi/client/GeneralLedgerApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceapi/client/GeneralLedgerApiClient.kt
@@ -16,10 +16,12 @@ class GeneralLedgerApiClient(private val transactionApi: TransactionControllerAp
       return transactionApi.getListOfTransactionsByAccountId(accountId).block()
         ?: throw IllegalStateException("Received null response when retrieving a list of transactions for account $accountId")
     } catch (e: WebClientResponseException) {
-      if (e.statusCode == HttpStatus.INTERNAL_SERVER_ERROR) {
+      if (e.statusCode == HttpStatus.NOT_FOUND) {
+        throw CustomException("Account not found", HttpStatus.NOT_FOUND, e)
+      } else if (e.statusCode == HttpStatus.INTERNAL_SERVER_ERROR) {
         throw CustomException("General Ledger Unavailable", HttpStatus.SERVICE_UNAVAILABLE, e)
       } else {
-        throw CustomException(e.message, e.statusCode, e)
+        throw CustomException("Unexpected error retrieving transactions", HttpStatus.INTERNAL_SERVER_ERROR, e)
       }
     }
   }
