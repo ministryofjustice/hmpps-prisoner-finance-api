0ae622d2bbab2609fcf049d08260f82d0c738c0a
tristan-madetech <tristan.moss@justice.gov.uk>
2026-03-05 16:45:23 +0000

    Updated client methods

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceapi/client/GeneralLedgerApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceapi/client/GeneralLedgerApiClient.kt
@@ -4,12 +4,27 @@ import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.prisonerfinanceapi.CustomException
+import uk.gov.justice.digital.hmpps.prisonerfinanceapi.clients.generalledger.AccountControllerApi
 import uk.gov.justice.digital.hmpps.prisonerfinanceapi.clients.generalledger.TransactionControllerApi
+import uk.gov.justice.digital.hmpps.prisonerfinanceapi.models.generalledger.AccountResponse
 import uk.gov.justice.digital.hmpps.prisonerfinanceapi.models.generalledger.PrisonerTransactionListResponse
 import java.util.UUID
 
 @Component
-class GeneralLedgerApiClient(private val transactionApi: TransactionControllerApi) {
+class GeneralLedgerApiClient(private val transactionApi: TransactionControllerApi, private val accountApi: AccountControllerApi) {
+
+  fun getAccountByRef(prisonerNumber: String): List<AccountResponse> {
+    try {
+      return accountApi.getAccounts(prisonerNumber).block()
+        ?: throw IllegalStateException("Received null response when retrieving account by reference $prisonerNumber")
+    } catch (e: WebClientResponseException) {
+      if (e.statusCode == HttpStatus.INTERNAL_SERVER_ERROR) {
+        throw CustomException("General Ledger Unavailable", HttpStatus.SERVICE_UNAVAILABLE, e)
+      } else {
+        throw CustomException("Unexpected error retrieving transactions", HttpStatus.INTERNAL_SERVER_ERROR, e)
+      }
+    }
+  }
 
   fun getListOfTransactionsByAccountId(accountId: UUID): List<PrisonerTransactionListResponse> {
     try {
